4b24f8bf335a875873f54a6f0348b19be2075b20
Parth_Dave_ <parth.itaims@gmail.com>
2024-09-05 07:22:01 +0530

    final commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css';
-import { Provider } from "react-redux";
-import { configureStore } from "./Store";
 
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
 
-createRoot(document.getElementById('root')!).render(
+createRoot(rootElement).render(
   <StrictMode>
-      <Provider store={configureStore({})}>
           <App />
-    </Provider>
   </StrictMode>,
 )
